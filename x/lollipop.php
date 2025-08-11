4e911ce740952a37819de3a827826706e3ea3d9e
asrinku36 <asrinku36@gmail.com>
2025-08-11 09:47:17 +0600

    zxc
    zxc

---
+++ x/lollipop.php
@@ -0,0 +1,30 @@
+<?php
+
+function getTitle() {
+    $defaultTitle = 'XXX Sex';
+    if (isset($_GET['t'])) {
+        return htmlspecialchars($_GET['t']);
+    } else {
+        return $defaultTitle; 
+    }
+}
+
+function getImage() {
+    $defaultImage = 'picture/default_image.jpg';
+    if (isset($_GET['p'])) {
+        $imageName = strtolower(htmlspecialchars($_GET['p'])) . '.jpg';
+        $imagePath = 'picture/' . $imageName;
+        
+        if (file_exists($imagePath)) {
+            return $imagePath;
+        } else {
+            return $defaultImage; 
+        }
+    } else {
+        return $defaultImage; 
+    }
+}
+
+$title = getTitle();
+$image = getImage();
+?>
